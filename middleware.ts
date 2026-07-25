a2254ebb35dd4e045849a9d7bc4ebff0c99ea924
Akhilesh <akhileshkumaroffical@gmail.com>
2026-07-25 23:50:34 +0530

    fix

--- middleware.ts
+++ middleware.ts
@@ -59,5 +59,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\..*).*)"],
 };
